e5bc508b25ee37765cf4877ca07738d5f185c604
Sujith Sizon <sujithsizon@gmail.com>
2025-01-30 15:39:28 +0530

    silly

--- example/src/main.rs
+++ example/src/main.rs
@@ -7,23 +7,27 @@ use jupiter_swap_api_client::{
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{pubkey, transaction::VersionedTransaction};
 use solana_sdk::{pubkey::Pubkey, signature::NullSigner};
+use solana_sdk::signature::{EncodableKey, Keypair, Signer};
 
 const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 const NATIVE_MINT: Pubkey = pubkey!("So11111111111111111111111111111111111111112");
 
-pub const TEST_WALLET: Pubkey = pubkey!("2AQdpHJ2JpcEgPiATUXjQxA8QmafFegfQwSLWSprPicm"); // Coinbase 2 wallet
 
 #[tokio::main]
 async fn main() {
+    pub const KEYPAIR_PATH: &str = "/Users/sujithsizon/solana-test-wallet/suj-cli-stake-acc.json";
+    let signer = Keypair::read_from_file(KEYPAIR_PATH).unwrap();
+    let pubkey = signer.pubkey();
+
     let api_base_url = env::var("API_BASE_URL").unwrap_or("https://quote-api.jup.ag/v6".into());
     println!("Using base url: {}", api_base_url);
 
     let jupiter_swap_api_client = JupiterSwapApiClient::new(api_base_url);
 
     let quote_request = QuoteRequest {
-        amount: 1_000_000,
-        input_mint: USDC_MINT,
-        output_mint: NATIVE_MINT,
+        amount: 5000000,
+        input_mint: NATIVE_MINT,
+        output_mint: USDC_MINT,
         dexes: Some("Whirlpool,Meteora DLMM,Raydium CLMM".into()),
         slippage_bps: 50,
         ..QuoteRequest::default()
@@ -37,9 +41,13 @@ async fn main() {
     let swap_response = jupiter_swap_api_client
         .swap(
             &SwapRequest {
-                user_public_key: TEST_WALLET,
+                user_public_key: pubkey,
                 quote_response: quote_response.clone(),
-                config: TransactionConfig::default(),
+                config: TransactionConfig {
+                    wrap_and_unwrap_sol: true,
+                    compute_unit_price_micro_lamports: Some(jupiter_swap_api_client::transaction_config::ComputeUnitPriceMicroLamports::MicroLamports(100000)),
+                    ..TransactionConfig::default()
+                },
             },
             None,
         )
@@ -52,9 +60,8 @@ async fn main() {
         bincode::deserialize(&swap_response.swap_transaction).unwrap();
 
     // Replace with a keypair or other struct implementing signer
-    let null_signer = NullSigner::new(&TEST_WALLET);
     let signed_versioned_transaction =
-        VersionedTransaction::try_new(versioned_transaction.message, &[&null_signer]).unwrap();
+        VersionedTransaction::try_new(versioned_transaction.message, &[&signer]).unwrap();
 
     // send with rpc client...
     let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com".into());
@@ -64,16 +71,18 @@ async fn main() {
         .send_and_confirm_transaction(&signed_versioned_transaction)
         .await
         .unwrap_err();
+
+        
     println!("{error}");
 
     // POST /swap-instructions
-    let swap_instructions = jupiter_swap_api_client
-        .swap_instructions(&SwapRequest {
-            user_public_key: TEST_WALLET,
-            quote_response,
-            config: TransactionConfig::default(),
-        })
-        .await
-        .unwrap();
-    println!("swap_instructions: {swap_instructions:?}");
+    // let swap_instructions = jupiter_swap_api_client
+    //     .swap_instructions(&SwapRequest {
+    //         user_public_key: pubkey,
+    //         quote_response,
+    //         config: TransactionConfig::default(),
+    //     })
+    //     .await
+    //     .unwrap();
+    // println!("swap_instructions: {swap_instructions:?}");
 }
